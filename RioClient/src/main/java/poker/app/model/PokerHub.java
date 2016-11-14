144e8f58d19a2d25bbb03da56024d10424742a9d
Arai <Arai@128.4.82.242>
2016-11-13 20:34:22 -0500

    Most of Part 4, Part 6 Complete

--- RioClient/src/main/java/poker/app/model/PokerHub.java
+++ RioClient/src/main/java/poker/app/model/PokerHub.java
@@ -7,9 +7,11 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import exceptions.DeckException;
 import netgame.common.Hub;
+import poker.app.view.RootLayoutController;
 import pokerBase.Action;
 import pokerBase.Card;
 import pokerBase.CardDraw;
@@ -51,18 +53,40 @@ public class PokerHub extends Hub {
 
 		if (message instanceof Action) {
 			
+			Action action = (Action) message;
+			switch (action.getAction()) {
+			case StartGame:
+				resetOutput();
+				// ?????
+				sendToAll(HubPokerTable);
+				break;
 			//TODO: If the Action = StartGame, start the game...
 			//		Create an instance of GamePlay, set all the parameters
-			
-			//TODO: If Action = Sit, add the player to the table
-			
-			//TODO: If Action = Leave, remove the player from the table
-			
-			//TODO: If Action = Sit or Leave, send the Table
+			case Sit:
+				resetOutput();
+				HubPokerTable.AddPlayerToTable(action.getPlayer());
+				sendToAll(HubPokerTable);
+				break;
+			//FINISHED: If Action = Sit, add the player to the table
+				
+			case Leave:
+				resetOutput();
+				HubPokerTable.RemovePlayerFromTable(action.getPlayer());
+				sendToAll(HubPokerTable);
+				break;
+			//FINISHED: If Action = Leave, remove the player from the table
+			case TableState:
+				resetOutput();
+				sendToAll(HubPokerTable);
+				break;
+			//FINISHED: If Action = Sit or Leave, send the Table
 			//		back to the client
-			
-			//TODO: If Action = GameState, send HubGamePlay 
+			case GameState:
+				sendToAll(HubGamePlay);
+				break;
+			//FINISHED: If Action = GameState, send HubGamePlay 
 			//		back to the client
+			}
 		}
 
 		System.out.println("Message Received by Hub");
